51339f5091ffe3b2b4f6f7561f5c672c049b9db8
Tapani Isoaho <tapani.isoaho@gmail.com>
2023-11-20 18:41:45 +0100

    Test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-# Token for access
+#Token for access
 with open('token.txt', 'r') as f:
     token = f.read()
 
-# GraphQL endpoint URL
+#GraphQL endpoint URL
 url = 'https://api.stratz.com/graphql'
-# Headers for the request
+
+#Headers for the request
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {}'.format(token)
@@ -15,7 +16,7 @@ headers = {
 
 def fetch_match_data(matchid):
     graphql_match_query = '''
-        query {  
+        query {
             match(id: %s) {
                 players {
                     steamAccountId
@@ -40,7 +41,7 @@ def fetch_match_data(matchid):
 
 def fetch_player_data(steamAccountId):
     graphql_player_query = '''
-        query {  
+        query {
             player(steamAccountId: %s) {
                 steamAccount {
                     name
@@ -56,17 +57,39 @@ def fetch_player_data(steamAccountId):
     response = requests.post(url, headers=headers, json=request_data).json()
     return response
 
-def process_response(response):
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        #print(json.dumps(data, indent=2))
-    else:
-        print(f"Error: {response.status_code}\n{response.text}")
-    return json.dumps(data, indent=2)
+def process_matches(match_list, players):
+    responses = {}
+    for player in players:
+        responses[player] = []
+    with open(match_list) as matches:
+        for match in matches:
+            match_name = match.strip()
+            response = fetch_match_data(match)
+            for player in (response["data"]["match"]["players"]):
+                if player["steamAccountId"] in players.values():
+                    result_temp = [match_name,player["imp"]]
+                    player_name =  {i for i in players if players[i]==player["steamAccountId"]}
+                    player_name = player_name.pop()
+                    result = responses[player_name]
+                    result.append(result_temp)
+                    responses[player_name] = result
+    return responses
+ 
+def find_player(player_list):
+    players_list = {}
+    with open(player_list) as players:
+        for player in players:
+            response = fetch_player_data(player)
+            id = response["data"]["player"]["steamAccount"]["id"]
+            name = response["data"]["player"]["steamAccount"]["name"]
+            players_list[name] = id
+    return players_list
 
 #main script
-with open('matchlist.txt') as matches:
-    for match in matches:
-        response = fetch_match_data(match)
-        print(response)
-        break
+def main():
+    players = find_player('playerlist.txt')
+    print(process_matches('matchlist.txt', players))
+
+if __name__ == "__main__":
+    main()
+
